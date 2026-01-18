7b787282cf5625d2ed3b2dedada28f25bda9467d
Pujiatmo Subarkah <pujiatmo.subarkah@gmail.com>
2026-01-18 21:57:36 +0700

    nambah bahasa

---
+++ components/ui/_exports/button/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    :class="buttonClasses"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { buttonVariants, type ButtonVariants } from './index'
+
+interface Props extends ButtonVariants {
+  asChild?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  variant: 'default',
+  size: 'default',
+})
+
+const buttonClasses = computed(() =>
+  buttonVariants({
+    variant: props.variant,
+    size: props.size,
+  })
+)
+</script>
